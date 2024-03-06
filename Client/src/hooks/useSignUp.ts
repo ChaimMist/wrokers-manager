2a5fe16393ab5c012f178611fa3b8c42738fdaa5
chaim <chaimschool@gmail.com>
2024-03-06 23:03:18 +0200

    added route gaurd

--- Client/src/hooks/useSignUp.ts
+++ Client/src/hooks/useSignUp.ts
@@ -1,6 +1,6 @@
-import {useMutation} from "react-query";
+import {useMutation, useQuery} from "react-query";
 import {User} from "../types/user";
-import {signIn, signUp} from "../api/userService/userServiceApi";
+import {getUser, signIn, signUp} from "../api/userService/userServiceApi";
 import {snackBarError, snackBarSuccess} from "../utils/snackBar/snackBars";
 import {AxiosError} from "axios";
 import {NavigateFunction, useNavigate} from "react-router-dom";
@@ -11,7 +11,7 @@ import {UserContextTypes} from "../types/userContextTypes";
 
 
 export const useSignUp = () => {
-    const navigate:NavigateFunction = useNavigate();
+    const navigate: NavigateFunction = useNavigate();
     return useMutation((user: User) => signUp(user), {
         onSuccess: (): void => {
             snackBarSuccess('User created successfully');
@@ -21,13 +21,32 @@ export const useSignUp = () => {
     });
 }
 
+export const useGetUser = () => {
+    const {dispatch} = useContext(UserContext) as UserContextTypes;
+    return useQuery('getUser', () => {
+        const token = sessionStorage.getItem('token') || localStorage.getItem('token');
+        if (!token) return;
+        return getUser(token);
+    }, {
+        enabled: !!sessionStorage.getItem('token') || !!localStorage.getItem('token'),
+        onSuccess: (res: User): void => {
+            if (!res.token) return;
+            dispatch({type: 'UPDATE_USER', payload: res});
+            sessionStorage.setItem('token', res.token);
+        }
+    });
+
+}
+
 export const useSignIn = () => {
-    const navigate:NavigateFunction = useNavigate();
+    const navigate: NavigateFunction = useNavigate();
     const {dispatch} = useContext(UserContext) as UserContextTypes;
     return useMutation((credentials: Credentials) => signIn(credentials), {
         onSuccess: (res: User): void => {
+            if (!res.token) return;
             snackBarSuccess('Login successful');
             dispatch({type: 'UPDATE_USER', payload: res});
+            sessionStorage.setItem('token', res.token);
             navigate('/home');
         },
         onError: (error: AxiosError) => snackBarError(error?.response?.data as string)
---
+++ Client/src/pages/loadingPage/loadingPage.tsx
@@ -0,0 +1,9 @@
+
+
+export const loadingPage = () => {
+    return (
+        <div>
+            <h1>Loading Page</h1>
+        </div>
+    )
+}
